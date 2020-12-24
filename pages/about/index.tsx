21b9c4d46ba2485ce7107aadb6520e34291dc7ef
Till Fitzke <till@fitzke.net>
2020-12-24 12:08:15 +0100

    added chakra-ui

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
-
+import NextLink from "next/link";
+import { Heading, Link, Flex } from "@chakra-ui/react";
 const AboutPage = () => {
     return (
-        <>
-            <h1>This is our about page</h1>
-            <Link href='/'>Go to back home</Link>
-        </>
+        <Flex flexDirection="column" alignItems="center" margin={4}>
+            <Heading as="h1" size="2xl" marginY="2rem">This is our about page</Heading>
+            <NextLink href='/'><Link>Go to back home</Link></NextLink>
+        </Flex>
     )
 }
 
